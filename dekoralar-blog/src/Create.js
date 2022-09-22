47494acc998f1fbf2cf92e9c79c4ba358ed1cdb2
kah <karinasantanaarq@gmail.com>
2022-09-22 17:24:27 +1000

    creating new article

--- dekoralar-blog/src/Create.js
+++ dekoralar-blog/src/Create.js
@@ -1,18 +1,55 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      setIsPending(false);
+      // history.go(1);
+      history.push("/");
+    });
+  };
+
   return (
     <div className="create">
       <h2>Add a new article</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Article title:</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <label>Article body:</label>
-        <textarea required></textarea>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
         <label>Article author:</label>
-        <select>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Article</button>
+        {!isPending && <button>Add Article</button>}
+        {isPending && <button disabled>Adding article...</button>}
       </form>
     </div>
   );
