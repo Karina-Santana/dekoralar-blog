161a2965c0e445923e1a52f3db4b44c5752caf91
kah <karinasantanaarq@gmail.com>
2022-09-22 11:36:12 +1000

    fetching data

--- dekoralar-blog/src/Home.js
+++ dekoralar-blog/src/Home.js
@@ -1,25 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "Lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "Lorem ipsum...", author: "luigi", id: 2 },
-    {
-      title: "Web dev tips",
-      body: "Lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
-
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" />
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === "mario")}
-        title="Mario's blogs"
-      />
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 };
